eb64bc3e0fed9afd7c63178c0ccfc797def1f843
wwei5743 <wwei5743@gmail.com>
2019-02-28 14:30:22 -0500

    Add files via upload

---
+++ FPS.py
@@ -0,0 +1,20 @@
+import datetime
+
+class FPS:
+    def __init__(self):
+        self._start = None
+        self._end = None
+        self._numFrames = 0
+    
+    def start(self):
+        self._start = datetime.datetime.now()
+        return self
+
+    def stop(self):
+        stop._end = datetime.datetime.now()
+    
+    def elapsed(self):
+        return (self._end - self._start).total_seconds()
+
+    def fps(self):
+        return self._numFrames/self.elapsed()
